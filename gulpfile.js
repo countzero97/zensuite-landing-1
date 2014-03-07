8d58328e2c38cdb97b745c4e02b9141d4c50e793
Federico Rampazzo <frampone@gmail.com>
2014-03-07 11:37:46 +0000

    unified landing

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,30 @@
-var gulp = require('gulp')
-  , swig = require("gulp-swig")
+var gulp = require('gulp'),
+    swig = require('gulp-swig')
 
-gulp.task('default', function() {
-  
-  var options = {
-    data: { 
-      tr: require('./data')
-    }, ext: ".html"
-  };
-  
-  return gulp.src("./template/*.html", { read : false})
-         .pipe(swig(options))
-         .pipe(gulp.dest("./"));
-});
+    gulp.task('default', function () {
+
+        var options = {},
+            languages = ['en', 'it'];
+
+        languages.forEach(function (language) {
+            options[language] = {
+                data: {
+                    tr: require('./languages/' + language)
+                },
+                ext: '.html'
+            };
+        });
+
+        gulp.src('./swig/*.html', {
+            read: false
+        })
+            .pipe(swig(options['en']))
+            .pipe(gulp.dest('./'))
+        
+        gulp.src('./swig/*.html', {
+            read: false
+        })
+            .pipe(swig(options['it']))
+            .pipe(gulp.dest('./it'));
+
+    });
